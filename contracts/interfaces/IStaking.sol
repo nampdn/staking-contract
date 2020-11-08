03f403985bff4ec42345d5fee803475f3edfe2fc
thangn <thangn.1411@gmail.com>
2020-11-08 08:51:42 +0700

    update interface

--- contracts/interfaces/IStaking.sol
+++ contracts/interfaces/IStaking.sol
@@ -3,9 +3,7 @@ pragma solidity >=0.5.0;
 
 
 interface IStaking {
-    function create() external;
-    function delegate() external;
-    function undelegate() external;
+    function createValidator() external;
     function finalize() external;
     function doubleSign() external;
     function mint() external;
--- contracts/interfaces/IValidator.sol
+++ contracts/interfaces/IValidator.sol
@@ -5,7 +5,6 @@ interface IValidator {
         uint64 commissionMaxChangeRate, uint64 minSelfDelegation) external payable;
     function update(uint64 commissionRate) external;
     function unjail() external;
-    function getMissedBlock() external;
     function allocateToken() external;
     function slash() external;
     function jail() external;
@@ -13,6 +12,9 @@ interface IValidator {
     function withdrawRewards() external;
     function withdrawCommission()external;
     function withdraw() external;
-    function undelegate() external;
+    function undelegate(uint64 amount) external;
+    function getCommissionRewards() external;
+    function getDelegationRewards(address delAddr) external;
+    function getDelegation(address delAddr) external;
 
 }
