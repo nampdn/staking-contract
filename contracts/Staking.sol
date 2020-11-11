a444051a2084089ecf526cc95fcf6a167cd871f5
thangn <thangn.1411@gmail.com>
2020-11-11 21:00:17 +0700

    implement ownable, init staking params

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,11 +1,12 @@
 pragma solidity >=0.5.0;
-import "./interfaces/IStaking.sol";
-import "./interfaces/IValidator.sol";
-import "./interfaces/IMinter.sol";
-import "./Validator.sol";
+import {IStaking} from "./interfaces/IStaking.sol";
+import {IValidator} from  "./interfaces/IValidator.sol";
+import {IMinter} from "./interfaces/IMinter.sol";
+import {Validator} from "./Validator.sol";
 import {SafeMath} from "./Safemath.sol";
+import {Ownable} from "./Ownable.sol";
 
-contract Staking is IStaking {
+contract Staking is IStaking, Ownable {
     using SafeMath for uint256;
     struct Params {
         uint256 baseProposerReward;
@@ -35,6 +36,22 @@ contract Staking is IStaking {
     mapping(address => address) public valOf;
     mapping(address => ValidatorState) private _validatorState;
 
+
+    constructor() public {
+        params = Params({
+            maxValidators: 100,
+            downtimeJailDuration: 600,
+            baseProposerReward: 1 * 10**16,
+            bonusProposerReward: 4 * 10**16,
+            slashFractionDowntime: 1 * 10**14,
+            unbondingTime: 1814400,
+            slashFractionDoubleSign: 5 * 10**16,
+            signedBlockWindow: 100,
+            minSignedPerWindow: 5 * 10**16
+        });
+    }
+
+
     // create new validator
     function createValidator(string calldata _name, uint64 _maxRate, uint64 _maxChangeRate, uint64 _minSelfDelegation) public{
         require(ownerOf[msg.sender] == address(0x0), "Valdiator owner exists");
@@ -50,7 +67,7 @@ contract Staking is IStaking {
     }
 
 
-    function finalize(address[] memory valAddr, uint64[] memory votingPower, bool[] memory signed) external{
+    function finalize(address[] memory valAddr, uint64[] memory votingPower, bool[] memory signed) external onlyOwner{
         uint256 previousTotalPower = 0;
         uint256 sumPreviousPrecommitPower = 0;
         for (uint256 i = 0; i < votingPower.length; i++) {
--- contracts/interfaces/IStaking.sol
+++ contracts/interfaces/IStaking.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 interface IStaking {
     function createValidator(uint64 commissionRate, uint64 commissionMaxRate, 
-        uint64 commissionMaxChangeRate, uint64 minSelfDelegation) external returns (address);
+        uint64 commissionMaxChangeRate, uint64 minSelfDelegation) external;
     function finalize(address[] memory vals, uint64[] memory votingPower, bool[] memory signed) external;
     function doubleSign(address valAddr, uint64 votingPower, uint64 height) external;
     function mint() external returns (uint64);
